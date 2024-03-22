fccdba3ca28337d1b03a8e4afc5665ea46c9841e
glucrypto <glucrypto@protonmail.com>
2024-03-22 08:27:02 -0400

    deployed simple account to align

--- deploy/2_deploy_SimpleAccountFactory.ts
+++ deploy/2_deploy_SimpleAccountFactory.ts
@@ -1,34 +1,39 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { DeployFunction } from 'hardhat-deploy/types'
-import { ethers } from 'hardhat'
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const provider = ethers.provider
-  const from = await provider.getSigner().getAddress()
-  const network = await provider.getNetwork()
+const deploySimpleAccountFactory: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const provider = ethers.provider;
+  const from = await provider.getSigner().getAddress();
+  const network = await provider.getNetwork();
   // only deploy on local test network.
 
-  const forceDeployFactory = process.argv.join(' ').match(/simple-account-factory/) != null
+  const forceDeployFactory =
+    process.argv.join(" ").match(/simple-account-factory/) != null;
 
-  if (!forceDeployFactory && network.chainId !== 31337 && network.chainId !== 1337) {
-    return
-  }
+  /* if (
+    !forceDeployFactory &&
+    network.chainId !== 31337 &&
+    network.chainId !== 1337
+  ) {
+    return;
+  } */
 
-  const entrypoint = await hre.deployments.get('EntryPoint')
-  await hre.deployments.deploy(
-    'SimpleAccountFactory', {
-      from,
-      args: [entrypoint.address],
-      gasLimit: 6e6,
-      log: true,
-      deterministicDeployment: true
-    })
-
-  await hre.deployments.deploy('TestCounter', {
+  const entrypoint = await hre.deployments.get("EntryPoint");
+  let ret = await hre.deployments.deploy("SimpleAccountFactory", {
+    from,
+    args: [entrypoint.address],
+    log: true,
+    deterministicDeployment: true,
+  });
+  console.log("==simpleaccountfactory addr=", ret.address);
+  /*  await hre.deployments.deploy("TestCounter", {
     from,
     deterministicDeployment: true,
-    log: true
-  })
-}
+    log: true,
+  }); */
+};
 
-export default deploySimpleAccountFactory
+export default deploySimpleAccountFactory;
